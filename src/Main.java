598d6b6da32d82540cae0880ad7ee0a2bdb8ac8f
abylay11 <abl_2023@outlook.com>
2023-12-27 18:32:08 +0500

    #6 – Conditional constructions (if-else, switch-case)

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,75 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        // Условные конструкции if-else
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите 1 число: ");
-        float num1 = scan.nextFloat();
+        int num1 = scanner.nextInt();
 
         System.out.print("Введите 2 число: ");
-        float num2 = scan.nextFloat();
+        int num2 = scanner.nextInt();
 
-        float res1 = num1 + num2;
-        float res2 = num1 - num2;
-        float res3 = num1 * num2;
-        float res4 = num1 / num2;
+        int res;
 
-        System.out.println("Результат: ");
-        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4);
+        System.out.print("Действие: ");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
 
+        switch (action) {
+            case "+":
+                res = num1 + num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Error");
+                } else {
+                    res = num1 / num2;
+                    System.out.println("Результат: " + res);
+                }
+                break;
+            default:
+                System.out.println("Не верное действие");
+        }
+/*
+        System.out.print("Введите роль: ");
+        String role = scanner.nextLine();
+        System.out.print("Введите пароль: ");
+        String pass = scanner.nextLine();
+        if (role.equals("Admin") && pass.equals("12345") )
+            System.out.println("Все пользователи");
+        else {
+            System.out.println("Привет как вас зовут?");
+            String name = scanner.nextLine();
+            System.out.println("Привет: " + name);
+        }
+*/
+/*
+        int num = scanner.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("Number is 1");
+                break;
+            case 2:
+                System.out.println("Number is 2");
+                break;
+            case 5:
+                System.out.println("Number is 5");
+                break;
+            case 10:
+                System.out.println("Number is 10");
+                break;
+            default:
+                System.out.println("Default");
+        }
+*/
     }
 }
