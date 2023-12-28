9ef02dce2a4d6a9eae8b9abb1e022045f12520f8
abylay11 <abl_2023@outlook.com>
2023-12-28 17:49:55 +0500

    #8 - Arrays of data. One-dimensional and multidimensional arrays.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,56 @@
 public class Main {
     public static void main(String[] args) {
-        // Циклы
-       /* for(float i = 100; i > 10; i /= 2) {
-            System.out.println("Element: " + i);
+        // Массивы данных
+        /*int[] nums = new int[5];
+        nums[0] = 45;
+        nums[1] = 21;
+        nums[2] = 32;
+        nums[3] = 24;
+        nums[4] = 55;
+
+        int res = nums[1] + nums[2];
+
+        System.out.println(res);
+
+        float[] nums2 = new float[] {5.0f, 6.45f, 89.984f};
+
+        for (int i = 0; i < nums2.length; i++) {
+            System.out.println("Element: " + nums2[i]);
         }*/
+/*
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[4];
 
-        /*int i = 0;
-        while (i <= 10) {
-            System.out.println("Element: " + i);
-            i++;
-        }*/
-       /* int i = 100;
-        do {
-            System.out.println("Element: " + i);
-            i *= 100;
-        } while (i < 10);*/
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Введите число: ");
+            int value = scanner.nextInt();
+            arr[i] = value;
+        }
 
-        for (int i = 5; i < 25; i+=2) {
-            if (i % 3 == 0) continue;
+        int min = arr[0];
+        for (int j : arr) {
+            if (j < min) min = j;
+        }
+        System.out.println("Minimal: " + min);*/
 
-            if (i >= 17) break;
-            System.out.println("Element: " + i);
+        // Многоиерные массивы
+
+        char[][] syms = new char[2][2];
+        syms[0][0] = 't';
+        System.out.println(syms[0][0]);
+
+        byte[][] nums = new byte[][] {
+                {1, 2, 3},
+                {2, 3, 5},
+                {1, 3, 5}
+        };
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print(nums[i][j]);
+                if (j != nums.length - 1) System.out.print(", ");
+            }
+            System.out.println();
         }
     }
 }
