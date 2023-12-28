dc4d0479cb0dd03813569b486f2d1a92485cab03
abylay11 <abl_2023@outlook.com>
2023-12-28 18:07:27 +0500

    #9 - Collections in Java. Collections Framework.

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        // Массивы данных
-        /*int[] nums = new int[5];
-        nums[0] = 45;
-        nums[1] = 21;
-        nums[2] = 32;
-        nums[3] = 24;
-        nums[4] = 55;
+        // Collections in Java
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(50);
+        numbers.add(1, 30);
 
-        int res = nums[1] + nums[2];
+        System.out.println(numbers.size());
+        System.out.println(numbers.get(1));
+        numbers.remove(1);
 
-        System.out.println(res);
+//        numbers.clear();
 
-        float[] nums2 = new float[] {5.0f, 6.45f, 89.984f};
-
-        for (int i = 0; i < nums2.length; i++) {
-            System.out.println("Element: " + nums2[i]);
-        }*/
-/*
-        Scanner scanner = new Scanner(System.in);
-        int[] arr = new int[4];
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print("Введите число: ");
-            int value = scanner.nextInt();
-            arr[i] = value;
+        for (Integer el : numbers) {
+            System.out.println(el);
         }
 
-        int min = arr[0];
-        for (int j : arr) {
-            if (j < min) min = j;
-        }
-        System.out.println("Minimal: " + min);*/
+        // Linked List
+        LinkedList<Float> numbers2 = new LinkedList<>();
+        numbers2.add(5.6f);
+        numbers2.add(15.6f);
+        numbers2.add(17.6f);
 
-        // Многоиерные массивы
-
-        char[][] syms = new char[2][2];
-        syms[0][0] = 't';
-        System.out.println(syms[0][0]);
-
-        byte[][] nums = new byte[][] {
-                {1, 2, 3},
-                {2, 3, 5},
-                {1, 3, 5}
-        };
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums[i].length; j++) {
-                System.out.print(nums[i][j]);
-                if (j != nums.length - 1) System.out.print(", ");
-            }
-            System.out.println();
+        for (Float el: numbers2) {
+            System.out.println(el);
         }
     }
 }
