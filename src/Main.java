e4ce22af4ef4e0674ffd2757a88a8da455fcff29
abylay11 <abl_2023@outlook.com>
2023-12-28 22:37:12 +0500

    #13 - Class constructors.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // ООП: классы и обьекты
-        Transport bmw = new Transport();
-        bmw.setValues(250.5f, 2500, "White", new byte[] {0,0,0});
-        String res1 = bmw.getValues();
-        System.out.println(res1);
-//        bmw.speed = 250.5f;
-//        bmw.weight = 2500;
-//        bmw.color = "White";
-//        bmw.coordinate = new byte[] {0, 0, 0};
+        // ООП: Конструкторы класса
+        Transport bmw = new Transport(250.5f, 2500, "White", new byte[] {0,0,0});
 
-        Transport truck = new Transport();
-        truck.speed = 140.5f;
-        truck.weight = 5600;
-        truck.color = "Red";
-        truck.coordinate = new byte[] {100, 0, 100};
-
-//        System.out.println("Truck speed: " + truck.speed + ". BMW speed: " + bmw.speed);
-        String res2 = truck.getValues();
-        System.out.println(res2);
+        Transport truck = new Transport(5600, new byte[] {100, 0, 100});
     }
 }
--- src/Transport.java
+++ src/Transport.java
@@ -1,20 +1,33 @@
 public class Transport {
-    public float speed;
-    public int weight;
-    public String color;
-    public byte[] coordinate;
+    private float speed;
+    private int weight;
+    private String color;
+    private byte[] coordinate;
 
-    public void setValues(float _speed, int _weight, String _color, byte[] _coordinate) {
-        speed = _speed;
-        weight = _weight;
-        color = _color;
-        coordinate = _coordinate;
+    public Transport(float _speed, int _weight, String _color, byte[] _coordinate) {
+        // Join to DB
+        System.out.println("Object created");
+        this.setValues(_speed, _weight, _color, _coordinate);
+        System.out.println(this.getValues());
+    }
+
+    public Transport(int weight, byte[] coordinate) {
+        this.weight = weight;
+        this.coordinate = coordinate;
+        System.out.println(this.getValues());
+    }
+
+    public void setValues(float speed, int weight, String color, byte[] coordinate) {
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
     }
 
     public String getValues() {
-        String info = "Object speed: " + speed + ". Weight: " + weight + ". Color: " + color + ".\n";
+        String info = "Object speed: " + this.speed + ". Weight: " + this.weight + ". Color: " + this.color + ".\n";
         StringBuilder infoCoordinates = new StringBuilder("Coordinates:\n");
-        for (byte b : coordinate) infoCoordinates.append(b).append("\n");
+        for (byte b : this.coordinate) infoCoordinates.append(b).append("\n");
 
         return info + infoCoordinates;
     }
